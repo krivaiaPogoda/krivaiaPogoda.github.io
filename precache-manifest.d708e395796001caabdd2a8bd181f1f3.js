7d3f9e839ecf8c934cd86e90019e0a4c02a91271
krivaiaPogoda <48444765+krivaiaPogoda@users.noreply.github.com>
2019-03-11 16:37:15 +0200

    1

---
+++ precache-manifest.d708e395796001caabdd2a8bd181f1f3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1875d76e617b01b7e03fe1bdc19cf00",
+    "url": "/static/media/Thermometer.d1875d76.png"
+  },
+  {
+    "revision": "310e74525d0d70153b55",
+    "url": "/static/css/main.5f43865f.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "50d953b171875f452eda",
+    "url": "/static/js/2.fb6ee956.chunk.js"
+  },
+  {
+    "revision": "533034f47caf289bc8de4688e476b536",
+    "url": "/static/media/feeling.533034f4.png"
+  },
+  {
+    "revision": "310e74525d0d70153b55",
+    "url": "/static/js/main.668609c5.chunk.js"
+  },
+  {
+    "revision": "4f4d69e2bf96890587d320453d9eb826",
+    "url": "/static/media/humidity.4f4d69e2.png"
+  },
+  {
+    "revision": "a5e0e77dfab5be5dcab7c67f13a356f3",
+    "url": "/static/media/barometer.a5e0e77d.png"
+  },
+  {
+    "revision": "2185dd55f5877632cefa632c14bc43d6",
+    "url": "/static/media/wind.2185dd55.png"
+  },
+  {
+    "revision": "63d8c12f52d7c7b84209d1f94b4d72b4",
+    "url": "/static/media/bg.63d8c12f.jpg"
+  },
+  {
+    "revision": "08259ee79e99b826841a7ae323fb95b5",
+    "url": "/index.html"
+  }
+];
